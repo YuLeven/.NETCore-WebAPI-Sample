18fe16062ba57ccdaf6e5959dabeab6a2bd54fcb
Yuri Levenhagen <yurileven@gmail.com>
2018-12-10 18:40:11 -0200

    Improve on routing by using RESTful style naming for the action controllers (#2)

--- src/HaruGaKita.WebAPI/Controllers/SessionsController.cs
+++ src/HaruGaKita.WebAPI/Controllers/SessionsController.cs
@@ -8,34 +8,22 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 #pragma warning disable 1591
 namespace HaruGaKita.WebAPI.Controllers
 {
-    public class AuthenticationController : BaseController
+    public class SessionsController : BaseController
     {
-        public AuthenticationController(IMediator mediator) : base(mediator)
-        {
-        }
+        public SessionsController(IMediator mediator) : base(mediator)
+        { }
 
         /// <summary>
         /// Signs-in an user
         /// </summary>
         /// <param name="loginRequest">Houses the username and password request</param>
-        /// <returns code="201">A JWT token</returns>
-        /// <returns code="401">If the credentials are invalid</returns>
         [HttpPost]
         [ProducesResponseType(201)]
         [ProducesResponseType(400)]
         [ProducesResponseType(401)]
-        public async Task<ActionResult<OAuthCredentials>> Login([FromBody] LoginCommand loginRequest)
+        public async Task<ActionResult<OAuthCredentials>> Post([FromBody] LoginCommand loginRequest)
         {
             return await _mediator.Send(loginRequest);
-        }
-
-
-        [HttpPost]
-        [ProducesResponseType(201)]
-        [ProducesResponseType(400)]
-        public async Task<ActionResult<UserDto>> CreateAccount([FromBody] CreateAccountCommand request)
-        {
-            return await _mediator.Send(request);
         }
     }
 }
